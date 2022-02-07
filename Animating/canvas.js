5f90c0584700bd02e9966a26708595082d8e1b88
Blanc <1yoouoo@gmail.com>
2022-02-07 11:13:25 +0900

    여러개의 원 만들기
    원 하나의 정보를 클래스로 보관하고 this 메서드를 이용해서 찍어냈음

--- Animating/canvas.js
+++ Animating/canvas.js
@@ -4,41 +4,73 @@ let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+class Circle {
 
+    init() {
+        this.radius = 30;
+        this.x = Math.random() * (innerWidth - this.radius * 2) + this.radius ; 
+        this.y = Math.random() * (innerHeight - this.radius * 2) + this.radius ;
+        this.dx = (Math.random() - 0.5);
+        this.dy = (Math.random() - 0.5);
+    }
 
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.strokeStyle = 'blue';
+        ctx.stroke();
+    }
 
-//화면 밖에 끼이는 버그있음
+    move() {
+        if(this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
 
-let radius = 30;
-let x = Math.random() * (innerWidth - radius * 2) + radius ; 
-let y = Math.random() * (innerHeight - radius * 2) + radius ;
-let dx = (Math.random() - 0.5) * 8;
-let dy = (Math.random() - 0.5) * 8;
-function circle() {
-    requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
+        else if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
 
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-    ctx.strokeStyle = 'blue';
-    ctx.stroke();
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+}
+
+//여러개의 원을 만들어 보자
+function init() {
+    let c1 = new Circle();
+    let c2 = new Circle();
+
+    c1.init();
+    c2.init();
+
+    function animate() {
+        requestAnimationFrame(animate);
+        ctx.clearRect(0, 0, innerWidth, innerHeight);
+        c1.draw();
+        c1.move();
+        c2.draw();
+        c2.move();
+    }
+    animate();
+    // circle();
+}
+// init();
+
+let circle_list = [];
+
+for(let i=0; i<100; i++) {
+    let circle = new Circle();
+    circle.init()
+    circle_list.push(circle)
 }
 
 function animate() {
-    circle();
-
-    if(x + radius > innerWidth || x - radius < 0) {
-        dx = -dx;
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    for(let i=0; i<100; i++) {
+        let circle = circle_list[i];
+        circle.draw();
+        circle.move();
     }
-
-    else if (y + radius > innerHeight || y - radius < 0) {
-        dy = -dy;
-    }
-
-
-    x += dx;
-    y += dy;
-
 }
-
 animate();

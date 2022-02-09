5423a19bd4e21266f8edbf52dc168e1d9eccd8ca
Blanc <1yoouoo@gmail.com>
2022-02-09 10:47:38 +0900

    obstacle shadow
    장애물에 그림자를 넣고
    
    명암 차이를 둬서 3D느낌을 표현해 봄

--- 2/Gravity/canvas.js
+++ 2/Gravity/canvas.js
@@ -5,11 +5,11 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let colorArray = [
-    '#3D5AB3',
-    '#273859',
-    '#3C6AA6',
-    '#6CA6D9',
-    '#4F5759'
+    '#08495D',
+    '#0B7074',
+    '#0F8C83',
+    '#F18E65',
+    '#EF746B'
 ]
 let mouse = {
     x : undefined,
@@ -31,21 +31,41 @@ window.addEventListener('resize', function(){
 
 class Obstacle {
     
-    init() {
-        this.x = 90
-        this.y = 600
-        this.width = 400
-        this.height = 60
-        this.shadowColor = 'black';
-        this.shadowBlur = 5;
+    init(x, y, width, height) {
+        this.x = x
+        this.y = y 
+        this.width = width 
+        this.height = height 
+        
     }
-
     draw() {
-        ctx.fillStyle = "#756E5D";
-        ctx.shadowColor = this.shadowColor;
-        ctx.shadowBlur = this.shadowBlur;
+        const xGap = 75
+        const yGap = 50
+
+        ctx.fillStyle = "#A86EC4";
         ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.shadowBlur = 0;
+        
+        
+        
+        ctx.fillStyle = "#520778";
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x- xGap, this.y + yGap);
+        ctx.lineTo(this.x- xGap, this.y + this.height + yGap);
+        ctx.lineTo(this.x, this.y + this.height);
+        ctx.fill();
+        
+        
+        ctx.fillStyle = "#091437";
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y + this.height);
+        ctx.lineTo(this.x - xGap, this.y + this.height + yGap);
+        ctx.lineTo(this.x + this.width - xGap, this.y + this.height + yGap);
+        ctx.lineTo(this.x + this.width, this.y + this.height);
+        ctx.fill();
+        
+        
+        // ctx.stroke();
     }
 }
 
@@ -103,7 +123,7 @@ function animate() {
     requestAnimationFrame(animate);
     
     ctx.clearRect(0, 0, innerWidth, innerHeight);
-    obstacle.init();
+    obstacle.init(200, 400, 360, 20);
     obstacle.draw();
     
     for(let i=0; i<100; i++) {

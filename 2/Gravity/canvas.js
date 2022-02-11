27eaa51a95ea4c6925bec94ebd71bebef49afd80
Blanc <1yoouoo@gmail.com>
2022-02-11 13:36:09 +0900

    add Obstacle bounce
    장애물에 튕기는 효과를 넣었는데
    
    변수의 스코프 문제로 상수로 집어넣어서 구현은 했지만 변수 스코프 다시 설정해서 상수 대신 obstacle 좌표를 넣어야함

--- 2/Gravity/canvas.js
+++ 2/Gravity/canvas.js
@@ -31,6 +31,7 @@ window.addEventListener('resize', function(){
 })
 
 class Board {
+    
     init() {
         let circleList = [];
         let obstacle = new Obstacle();
@@ -48,13 +49,13 @@ class Board {
             let circle = circleList[i];
             let x = Math.random() * (innerWidth - maxRadius * 2) + maxRadius;
             let y = Math.random() * (innerHeight - maxRadius * 2) + maxRadius;
-           
-
+            
+            
             // 겹침
             if((obstacle.x - maxRadius < x && obstacle.x + obstacle.width + maxRadius > x) && (obstacle.y - maxRadius < y && obstacle.y + obstacle.height + maxRadius > y)){
-                    x += obstacle.width + maxRadius * 2
-                    y += obstacle.height + maxRadius * 2
-                }
+                x += obstacle.width + maxRadius * 2
+                y += obstacle.height + maxRadius * 2
+            }
             circle.init(x, y);
         }
 
@@ -68,7 +69,7 @@ class Board {
                 let circle = circleList[i];
                 circle.draw();
                 
-                // circle.move();
+                circle.move();
             }
         }
 
@@ -104,7 +105,7 @@ class Obstacle {
         ctx.fill();
         
         
-        ctx.fillStyle = "#091437";
+        ctx.fillStyle = "#3E3758";
         ctx.beginPath();
         ctx.moveTo(this.x, this.y + this.height);
         ctx.lineTo(this.x - xGap, this.y + this.height + yGap);
@@ -139,13 +140,17 @@ class Circle {
     
     move() {
         // 바닥에 충돌할 때
+
         if (this.x + this.radius + this.dx > innerWidth || this.x - this.radius < 0) {
             this.dx = -this.dx;
         }
         
         this.x += this.dx;
 
-        if (this.y + this.radius + this.dy > innerHeight) {
+        if (this.y + this.radius + this.dy > innerHeight 
+            || ((this.y + this.radius + this.dy > 400 ) && (this.y + this.radius + this.dy) < 420) 
+            && ((this.x + this.radius + this.dx > 200) && (this.x + this.radius + this.dx < 560 ))
+            ) {
             this.dy *= -airFriction ; // 공기 저항
             this.dx *= floorFriction; // 바닥 마찰력
         }
@@ -154,36 +159,9 @@ class Circle {
         }
         this.y += this.dy;
         
-        
     }
     
 }
-// let circleList = [];
-
-// for(let i=0; i<circleNumber; i++) {
-//     let circle = new Circle();
-//     circle.init();
-//     circleList.push(circle);
-// }
-
-// let obstacle = new Obstacle();
-
-
-// function animate() {
-//     requestAnimationFrame(animate);
-    
-//     ctx.clearRect(0, 0, innerWidth, innerHeight);
-//     obstacle.init(200, 400, 360, 20);
-//     obstacle.draw();
-    
-//     for(let i=0; i<circleNumber; i++) {
-//         let circle = circleList[i];
-//         circle.draw();
-//         // circle.move();
-//     }
-// }
 
 new Board().init()
 
-
-// animate();

2dbbc1450b34908b7dfbcf92e218189e632f99ff
Blanc <1yoouoo@gmail.com>
2022-02-12 11:14:19 +0900

    코드 정리

--- 2/Gravity/canvas.js
+++ 2/Gravity/canvas.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let colorArray = [
+const colorArray = [
     '#08495D',
     '#0B7074',
     '#0F8C83',
@@ -12,13 +12,13 @@ let colorArray = [
     '#EF746B'
 ]
 
-const maxRadius = 20
-const minRadius = 2
-let circleNumber = 100
+const maxRadius = 20;
+const minRadius = 2;
+let circleNumber = 100;
 
-let gravity = 0.97
-let airFriction = 0.90
-let floorFriction =0.99
+let gravity = 0.97;
+let airFriction = 0.90;
+let floorFriction =0.99;
 
 
 window.addEventListener('resize', function(){
@@ -37,20 +37,17 @@ class Board {
             circleList.push(circle);
         }
 
-
         obstacle.init(200, 400, 360, 20);
 
         for(let i=0; i<circleNumber; i++) {
-            // debugger;
             let circle = circleList[i];
             let x = Math.random() * (innerWidth - maxRadius * 2) + maxRadius;
             let y = Math.random() * (innerHeight - maxRadius * 2) + maxRadius;
             
             
-            // 겹침
             if((obstacle.x - maxRadius < x && obstacle.x + obstacle.width + maxRadius > x) && (obstacle.y - maxRadius < y && obstacle.y + obstacle.height + maxRadius > y)){
-                x += obstacle.width + maxRadius * 2
-                y += obstacle.height + maxRadius * 2
+                x += obstacle.width + maxRadius * 2;
+                y += obstacle.height + maxRadius * 2;
             }
             circle.init(x, y);
         }
@@ -63,8 +60,8 @@ class Board {
             
             for(let i=0; i<circleNumber; i++) {
                 let circle = circleList[i];
+
                 circle.draw();
-                
                 circle.move(obstacle);
             }
         }
@@ -77,15 +74,15 @@ class Board {
 class Obstacle {
     
     init(x, y, width, height) {
-        this.x = x
-        this.y = y 
-        this.width = width 
-        this.height = height 
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
         
     }
     draw() {
-        const xGap = 75
-        const yGap = 50
+        const xGap = 75;
+        const yGap = 50;
 
         ctx.fillStyle = "#A86EC4";
         ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -135,8 +132,6 @@ class Circle {
     }
     
     move(obstacle) {
-        // 바닥에 충돌할 때
-
         if (this.x + this.radius + this.dx > innerWidth || this.x - this.radius < 0) {
             this.dx = -this.dx;
         }
@@ -147,17 +142,16 @@ class Circle {
             || ((this.y + this.radius + this.dy > obstacle.y ) && (this.y + this.radius + this.dy) < (obstacle.y + obstacle.height)) 
             && ((this.x + this.radius + this.dx > obstacle.x) && (this.x + this.radius + this.dx < (obstacle.x + obstacle.width) ))
             ) {
-            this.dy *= -airFriction ; // 공기 저항
-            this.dx *= floorFriction; // 바닥 마찰력
+            this.dy *= -airFriction ;
+            this.dx *= floorFriction;
         }
         else {
             this.dy += gravity;
         }
         this.y += this.dy;
-        
     }
     
 }
 
-new Board().init()
+new Board().init();
 

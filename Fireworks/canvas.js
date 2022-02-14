414be9527504ecfe243180ddfb796a2bcd9c9e14
Blanc <1yoouoo@gmail.com>
2022-02-14 10:41:03 +0900

    Fireworks - 마무리
    gravity, friction, opacity 적용
    
    css의 background-color 지우고 fillstyle에 rgba를 적용해서 좀 더 느낌있게 표현
    
    각각의 원들의 속도를 삼각함수를 이용해서 사방팔방으로 터지는 효과 적용
    
    foreach 인덱스와 splice함수를 이용해서 선명도가 사라지면 리스트에서 빼버림으로써 렉을 잡았음

--- Fireworks/canvas.js
+++ Fireworks/canvas.js
@@ -4,61 +4,84 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})
+
 const mouse = {
     x : innerWidth,
     y : innerHeight
 }
-
-class Circle {
-    init() {
-        this.radius = 10;
-        this.x = mouse.x;
-        this.y = mouse.y;
-        this.dx = Math.random() - 0.5
-        this.dy = Math.random() - 0.5
-
-        this.color = '#3D5AB3';
-    }
-
-    draw(){
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        ctx.strokeStyle = 'white';
-        ctx.fillStyle = this.color;
-        ctx.fill();
-        ctx.stroke();
-    }
-
-    move(){
-        this.x += this.dx
-        this.y += this.dy
-    }
-}
-// debugger
-let circles = []
-
-addEventListener('click', event => {
-    mouse.x = event.clientX
-    mouse.y = event.clientY
-    for(let i=0; i<5; i++) {
-        let circle = new Circle();
-        circle.init()
-        circles.push(circle)
-        console.log(mouse.x, mouse.y, circle, circles)
-    }
-})
-animate();
+const gravity = 0.05;
+const friction = 0.999;
+const circleNumber = 500;
+const angleIncrement = (Math.PI * 2) / circleNumber;
+const power = 7
+let opacity = 1;
+let circles = [];
 
 function animate() {
     requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
-
-    for(let i=0; i<5; i++) {
-        let circle = circles[i];
-        circles.forEach(circle => {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    circles.forEach((circle, i) => {
+        if(circle.opacity > 0){
             circle.draw();
-            circle.move();
-            
-        });
+            circle.move(circle.dx, circle.dy);
+        } else {
+            circles.splice(i, 1);
+        }
+    }
+)}
+
+class Circle {
+    init(dx, dy, opacity) {
+        this.radius = 3;
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.dx = dx
+        this.dy = dy
+        this.color = `hsl(${Math.random() * 360}, 50%, 50%)`;
+        this.opacity = opacity;
+    }
+    
+    draw(){
+        ctx.save()
+        ctx.strokeStyle = this.color;
+        ctx.fillStyle = this.color;
+        ctx.globalAlpha = this.opacity;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fill();
+        ctx.stroke();
+        //end
+        ctx.restore()
+    }
+    
+    move(dx, dy){
+        this.opacity = Math.max(0, this.opacity - 0.005)
+        this.dy += gravity
+        this.dx *= friction
+        this.dy *= friction
+        this.x += dx
+        this.y += dy
     }
 }
+
+
+addEventListener('click', event => {
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+    for(let i=0; i<circleNumber; i++) {
+        let circle = new Circle();
+        let dx = (Math.cos(angleIncrement * i) * Math.random()) * power;
+        let dy = (Math.sin(angleIncrement * i) * Math.random()) * power;
+        circle.init(dx, dy, opacity);
+        circles.push(circle);
+    }
+    console.log(circles)
+})
+animate();
+
